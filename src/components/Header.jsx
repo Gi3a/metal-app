09922d2214ffa0c8610c6359db352485226bd4f4
Mikhail <ialexei41@gmail.com>
2022-06-07 13:17:03 +0300

    v0.2 start of the main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 function Header() {
 	return (
-		<div>Header</div>
+		<Post>
+			<div>
+				<p>+ 7 (917) 933-34-53</p>
+				<p>salleleads@yandex.ru</p>
+
+				<button>Вход</button>
+				<button>Регистрация</button>
+			</div>
+			<div>
+				<div>
+					<h2>Металлообработки.рф</h2>
+					<p>Челябинская область</p>
+				</div>
+				<Link to='#'>Заказы</Link>
+				<Link to='#'>Разместить заказ</Link>
+				<button>заказы</button>
+				<button>Разместить заказ</button>
+			</div>
+		</Post>
 	)
 }
+
+const Post = styled.div`
+
+`
 
 export default Header
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+import Header from '../components/Header'
 
 function Home() {
 	return (
-		<div>Home</div>
+		<Post>
+			<Header />
+		</Post>
 	)
 }
+
+const Post = styled.div`
+	.wow {
+		background-color: red;
+	}
+`
 
 export default Home
