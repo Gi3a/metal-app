33637c4d9e7f194fce4af15cf4647e261be1d8a7
Mikhail <ialexei41@gmail.com>
2022-06-09 16:54:23 +0300

    v0.3 modal auth complete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,50 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import '../assets/styles/components/Modal.css'
-import { setState } from '../redux/modal/reducer';
-import Order from '../assets/images/orders/home-order1.png'
-
+import { useForm } from 'react-hook-form'
+import { setState } from '../redux/modal/reducer'
 
 function Modal() {
-
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+		reset,
+	} = useForm({
+		mode: 'onBlur'
+	});
 	const dispatch = useDispatch()
+
+	const onSubmit = (data) => {
+		alert(JSON.stringify(data))
+		reset()
+	}
 
 	return (
 		<div>
 
-			<div class="hystmodal" id="myModal">
-				<div class="hystmodal__window">
-					<button onClick={() => dispatch(setState())} data-hystclose class="hystmodal__close">Close</button>
-					Текст модального окошка.
-					<img src={Order} alt="Изображение в окне" />
+			<div className="hystmodal" id="myModal">
+				<div className="hystmodal__window">
+					<div className='modal-center'>
+						<button onClick={() => dispatch(setState())} data-hystclose class="hystmodal__close">Close</button>
+						<h2>Авторизация</h2>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<label>
+								Your phone:
+								<input {...register('phone', { required: true, minLength: { value: 9, message: "Некорректный номер" } })} />
+							</label>
+							<div style={{ height: 40 }}>
+								{errors?.phone && <p>Поле обязательно к заполнению!</p>}
+							</div>
+							<label>
+								Your password:
+								<input {...register('password', { required: true, minLength: { value: 5, message: "Некорректный пароль" } })} />
+							</label>
+							<label>
+								<input type="submit" />
+							</label>
+						</form>
+					</div>
 				</div>
 			</div>
 
